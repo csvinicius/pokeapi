0de3f6ca281eb0129102fb1c805409c2f42b865a
Vinicius Sousa <csousa.vinicius@gmail.com>
2024-07-15 09:49:56 -0300

    new: search bar and order by name filters

--- src/app/components/berries/berries.component.ts
+++ src/app/components/berries/berries.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { AppService } from '../../app.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { AppService } from '../../app.service';
 
 @Component({
   selector: 'app-berries',
   standalone: true,
-  imports: [CommonModule, NgxPaginationModule],
+  imports: [CommonModule, FormsModule, NgxPaginationModule],
   templateUrl: './berries.component.html',
   styleUrl: './berries.component.scss'
 })
 export class BerriesComponent implements OnInit {
   berriesList: any[] = [];
+  filteredBerriesList: any[] = [];
+  searchTerm: string = '';
+  sort: boolean = true;
   page: number = 1;
   
   private readonly berryUrl: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/berries';
@@ -26,6 +30,21 @@ export class BerriesComponent implements OnInit {
     this.appService.getBerries().subscribe((response: any) => {
       this.berriesList = response.results;
       this.berriesList.forEach((berry: any) => berry.imgUrl = `${this.berryUrl}/${berry.name}-berry.png`);
+      this.filteredBerriesList = this.berriesList
     });
+  }
+
+  filterBerries(): void {
+    this.filteredBerriesList = this.berriesList
+      .filter(item => item.name.toLowerCase().includes(this.searchTerm.toLowerCase()))
+      .sort((a, b) => {
+        const comparison = a.name.localeCompare(b.name);
+        return this.sort ? comparison : -comparison;
+      });
+  }
+
+  toggleSort(): void {
+    this.sort = !this.sort;
+    this.filterBerries();
   }
 }
